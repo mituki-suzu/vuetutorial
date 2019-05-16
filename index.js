2bb68110859c65b136350b6094026a771224ddac
mituki-suzu <s.0x0.n_1209@ezweb.ne.jp>
2019-05-17 00:28:46 +0900

    change file

--- index.js
+++ index.js
@@ -20,8 +20,60 @@ var app3 = new Vue({
     }
 })
 
+// v-forで要素をリストで表現できる
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
+
+// v-onでイベントリスナ
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+// v-modelで双方向バインディング
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+// todo-item と呼ばれる新しいコンポーネントを定義
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+  
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+    groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+    }
+})
+
 // 直接ではなくても代入できる
 app.message = "I have a chenge message!"
 app2.message = 'some new message'
 // v-ifはtrue,falseで表示非表示を切り替えられる
 app3.seen = false
+// pushで要素の追加
+app4.todos.push({ text: 'New item' })
